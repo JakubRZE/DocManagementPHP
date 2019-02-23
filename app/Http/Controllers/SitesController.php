17f262a17736bd9bb5d2dadda57a13d44c09ea47
JakubRZE <jakub.rzm@gmail.com>
2019-02-23 23:15:01 +0100

    Initial commit

---
+++ app/Http/Controllers/SitesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SitesController extends Controller
+{
+    public function Index()
+    {
+        echo 'Hello World';
+    }
+
+    public function Add()
+    {
+        return view('sites.add');
+    }
+}
---
+++ resources/views/sites/add.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title','Nazwa TESTOWA')
+
+@section('content')
+
+oguem
+
+@endsection
